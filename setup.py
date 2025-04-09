d99571d3f614c325a13619e36a9b479ae1c90293
Floating-Ocean <sea113290980@qq.com>
2025-04-09 16:21:15 +0800

    feat: 完善setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name='Easy-Pixie',
-    version='0.0.1',
-    author='Floating Ocean',
-    author_email='sea113290980@gmail.com',
-    license='MIT',
-    packages=['easy_pixie'],
-    description='A tool to simplify the use of python graphic library pixie-python.',
-    install_requires=[
-        'pixie-python>=4.3.0'
-    ]
-)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name="Easy-Pixie",
+                 version="0.0.1",
+                 author="Floating Ocean",
+                 author_email="sea113290980@gmail.com",
+                 description="A tool to simplify the use of python graphic library pixie-python.",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/Floating-Ocean",
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 python_requires='>=3.10.0',
+                 packages=setuptools.find_namespace_packages(
+                     include=["easy_pixie"], ),
+                 install_requires=['pixie-python>=4.3.0'],
+                 include_package_data=True)
